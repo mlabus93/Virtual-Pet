299cfd3e8140d70e2a2a52ae7e540f682301595b
mlabus93 <mlabus93@gmail.com>
2015-11-10 18:21:29 -0500

    added stats click toggle functionality

---
+++ Assets/UIController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class UIController : MonoBehaviour {
+
+    public Button statsBtn;
+    public Button outfitBtn;
+    public Button miniGameBtn;
+    public Button settingsBtn;
+    public GameObject statsPanel;
+    public GameObject canvas;
+    public Slider healthSlider;
+    public Slider happinessSlider;
+    public Slider fatigueSlider;
+    public Slider hungerSlider;
+    public Slider thirstSlider;
+    public Slider bladderSlider;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+    public void ShowStats()
+    {
+        if (statsBtn.gameObject.activeSelf)
+        {
+            HideStatsBtn();
+            ShowStatsPanel();
+        }
+    }
+
+    private void ShowStatsPanel()
+    {
+        statsPanel.gameObject.SetActive(true);
+    }
+    
+    private void HideStatsPanel()
+    {
+        statsPanel.gameObject.SetActive(false);
+    }
+
+    private void ShowStatsBtn()
+    {
+        statsBtn.gameObject.SetActive(true);
+    }
+	
+    private void HideStatsBtn()
+    {
+        statsBtn.gameObject.SetActive(false);
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        if (Input.GetButtonDown("Fire1") && !(statsBtn.gameObject.activeSelf))
+        {
+            HideStatsPanel();
+            ShowStatsBtn();
+        }
+    }
+}
