6fd2e360cf9360be2fcceb4d485afc331096f11d
mlabus93 <mlabus93@gmail.com>
2015-11-12 17:27:03 -0500

    UI fixed view/functionality.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -15,7 +15,10 @@ public class CameraFollow : MonoBehaviour {
 
     void FixedUpdate()
     {
-        Vector3 targetCamPos = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        if (target != null)
+        {
+            Vector3 targetCamPos = target.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        }
     }
 }
