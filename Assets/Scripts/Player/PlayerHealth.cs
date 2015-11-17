0ac3da9701199cdd95f99a9edcc3f69dc42246e2
jpmirvil <jmirvil@knights.ucf.edu>
2015-11-17 10:47:18 -0500

    Fixing a runtime error - will implement further changes on desktop

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,7 +6,7 @@ public class PlayerHealth : MonoBehaviour
 {
     public int startingHealth = 100;
     public int currentHealth;
-    public Slider healthSlider;
+    //public Slider healthSlider;
     public Image damageImage;
     public AudioClip deathClip;
     public float flashSpeed = 5f;
@@ -51,7 +51,7 @@ public class PlayerHealth : MonoBehaviour
 
         currentHealth -= amount;
 
-        healthSlider.value = currentHealth;
+        //healthSlider.value = currentHealth;
 
         playerAudio.Play ();
 
