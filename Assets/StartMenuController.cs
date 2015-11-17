1bc6868deda2095d70d787bab3d066dd8fb7d6d5
mlabus93 <mlabus93@gmail.com>
2015-11-17 17:03:17 -0500

    added character select and start menu scenes

---
+++ Assets/StartMenuController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartMenuController : MonoBehaviour {
+
+    public GameObject settingsPanel;
+    public Button continueBtn;
+    public Button newGameBtn;
+    public Button settingsBtn;
+    public Button closeOptionsBtn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void ShowSettings()
+    {
+        settingsPanel.gameObject.SetActive(true);
+    }
+
+    public void HideSettings()
+    {
+        settingsPanel.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
