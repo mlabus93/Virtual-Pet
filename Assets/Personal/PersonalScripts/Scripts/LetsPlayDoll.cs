f603bd5de413ad50b81298a13b872c8cdd1f7ef8
jpmirvil <jmirvil@knights.ucf.edu>
2015-11-23 11:56:58 -0500

    updating playdoll

--- Assets/Personal/PersonalScripts/Scripts/LetsPlayDoll.cs
+++ Assets/Personal/PersonalScripts/Scripts/LetsPlayDoll.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 namespace PersonalScripts
 {
@@ -14,9 +15,10 @@ namespace PersonalScripts
 
         void OnMouseDown()
         {
-            player.GetComponent<MoveToAction>().PlayWithDoll();
-
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                player.GetComponent<MoveToAction>().PlayWithDoll();
+            }
         }
     }
 }
-
